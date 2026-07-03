8e3d1576bc6344ddefcf0b34fe66033a352423dc
Amar Jaiswal <aj.jaiswal007@gmail.com>
2026-07-03 23:58:48 +0530

    Add tools

--- app/tools/scraper.py
+++ app/tools/scraper.py
@@ -1,6 +1,29 @@
 from bs4 import BeautifulSoup
-import requests
+import aiohttp
+from langchain_core.tools import tool
 
 
-def fetch_webpage(url: str) -> str:
-    requests.get(url=url)
+@tool
+async def fetch_webpage(url: str) -> str:
+    """Fetches the webpage content and returns a clean text for the page
+
+    Args:
+        url (str): URL to fetch
+
+    Returns:
+        str: Webpage content
+    """
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, ssl=False) as response:
+            response = await response.text()
+
+    soup = BeautifulSoup(response, "html.parser")
+    # 1. Isolate the body element
+    body = soup.find("body")
+
+    # 2. Remove boilerplate/extraneous tags that AI doesn't need
+    for element in body(["script", "style", "nav", "footer", "header"]):
+        element.decompose()
+
+    # 3. Extract the clean text
+    return body.get_text(separator="\n", strip=True)
